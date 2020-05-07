2821295091a61f7dc0ff23f58d3893ada7208d85
Erik <itserikanderson@gmail.com>
2020-05-07 14:04:21 -0400

    Initial Commit

---
+++ trpclGthc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  trpclGthc
+//
+//  Created by Erik Anderson on 5/7/20.
+//  Copyright © 2020 Erik Anderson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
